78a3462482737dce669b2a5d3c7bbb851795e077
DHIVYALOSHNI <dhv2404@gmail.com>
2025-08-04 20:48:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(_name_)
+
+# Load your trained ML model
+with open('trained_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+# Home page route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Prediction route
+@app.route('/predict', methods=['POST'])
+def predict():
+    nitrogen = float(request.form['nitrogen'])
+    phosphorus = float(request.form['phosphorus'])
+    potassium = float(request.form['potassium'])
+    temperature = float(request.form['temperature'])
+    humidity = float(request.form['humidity'])
+    ph_value = float(request.form['ph_value'])
+    rainfall = float(request.form['rainfall'])
+
+    # Prepare input for prediction
+    input_data = np.array([[nitrogen, phosphorus, potassium, temperature, humidity, ph_value, rainfall]])
+    prediction = model.predict(input_data)[0]
+
+    return render_template('index.html', prediction_text=f'Predicted Price: ₹{round(prediction, 2)} per quintal')
+
+if _name_ == '_main_':
+    app.run(debug=True)
